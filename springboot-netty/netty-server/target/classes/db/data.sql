08d7b235cc5b2925722ba404844eae8a9559dfd3
15626499648 <645359637@qq.com>
2020-02-19 14:58:22 +0800

    删除文件夹

--- springboot-netty/netty-server/target/classes/db/data.sql
+++
@@ -1,6 +0,0 @@
-
- DROP TABLE IF EXISTS message_info;
-
- CREATE TABLE IF NOT EXISTS message_info(id varchar(50) unsigned not null primary key,content varchar(10),create_time DATE);
-
- INSERT INTO message_info(id, content, create_time) VALUES ('1', 'tom：哈哈哈', parsedatetime('17-09-2012 18:47:52.69', 'dd-MM-yyyy hh:mm:ss.SS'))
